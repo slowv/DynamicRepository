54945445902b1557219dc7355c6471f5e4b4fb06
SlowV ❤ H3yae <31509114+slowv@users.noreply.github.com>
2023-06-16 17:59:26 +0700

    Init project

---
+++ src/main/java/com/vsoftlight/dynamicrepository/DynamicRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.vsoftlight.dynamicrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicRepositoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vsoftlight/dynamicrepository/DynamicRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsoftlight.dynamicrepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicRepositoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
